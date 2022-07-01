c2efe209baaa35fd7ed9bd641d81efacf72229eb
Archish <archish17@gmail.com>
2022-07-01 17:11:38 +0530

    added CIFAR10 scripts

--- cifar10/setup.sh
+++ cifar10/setup.sh
@@ -8,5 +8,4 @@ gdown --id 1wdyVIQLAUyzaJhYiJS6oRkc39MApGOtL
 wget https://github.com/Xerefic/Deep_Networks/releases/download/dataset/cifar-10.zip
 unzip ./cifar-10.zip
 rm -rf ./cifar-10.zip
-mv ./cifar-10/sample_submission.csv ./cifar-10/test_labels.csv
 cd ..
