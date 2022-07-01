d5de94a8a9ab119c64858859b8e574790b961d51
Archish <archish17@gmail.com>
2022-07-01 21:32:29 +0530

    added CIFAR10 scripts

--- cifar10/setup.sh
+++ cifar10/setup.sh
@@ -4,7 +4,7 @@ mkdir checkpoints
 pip3 install -r requirements.txt
 
 cd data
-gdown --id 1wdyVIQLAUyzaJhYiJS6oRkc39MApGOtL
+gdown --id 1_uPa6fE2wnrFi82vbk4gGaoK3h0fOcUl
 wget https://github.com/Xerefic/Deep_Networks/releases/download/dataset/cifar-10.zip
 unzip ./cifar-10.zip
 rm -rf ./cifar-10.zip
