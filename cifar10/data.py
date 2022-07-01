d95152201f571166fd5b8bb3b489751477f018a1
Archish <archish17@gmail.com>
2022-07-01 16:56:45 +0530

    added CIFAR10 scripts

--- cifar10/data.py
+++ cifar10/data.py
@@ -1,27 +1,29 @@
 from imports import *
 from args import *
 
-def process():
-    data = pd.read_csv(os.path.join(args.root_dir, 'train_labels.csv'))
-    encoder = preprocessing.LabelEncoder()
-    encoder.fit(data['label'])
-    return encoder
+# def process():
+#     data = pd.read_csv(os.path.join(args.root_dir, 'train_labels.csv'))
+#     encoder = preprocessing.LabelEncoder()
+#     encoder.fit(data['Label'])
+#     return encoder
 
 def get_data(args):
-    encoder = process()
+    # encoder = process()
 
-    data = pd.read_csv(os.path.join(args.root_dir, 'train_labels.csv'))
-    files = data['id']
-    labels = encoder.transform(data['label'])
-    train_data_files, valid_data_files, train_data_label, valid_data_label = train_test_split(files, labels, test_size=0.05, shuffle=True, stratify=labels)
+    data = pd.read_csv(os.path.join(args.root_dir, 'train_file.csv'))
+    files = data['Image']
+    labels = data['Label'] # encoder.transform(data['label'])
+    train_data_files = files
+    train_data_labels = labels
+    # train_data_files, valid_data_files, train_data_label, valid_data_label = train_test_split(files, labels, test_size=0.05, shuffle=True, stratify=labels)
     
-    data = pd.read_csv(os.path.join(args.root_dir, 'test_labels.csv'))
-    test_data_files = data['id']
-    test_data_label = encoder.transform(data['label'])
+    data = pd.read_csv(os.path.join(args.root_dir, 'test_file.csv'))
+    test_data_files = data['Image']
+    test_data_labels = data['Label'] # encoder.transform(data['label'])
 
-    train_data = pd.DataFrame({'id': train_data_files, 'label': train_data_label})
-    valid_data = pd.DataFrame({'id': valid_data_files, 'label': valid_data_label})
-    test_data = pd.DataFrame({'id': test_data_files, 'label': test_data_label})
+    train_data = pd.DataFrame({'id': train_data_files, 'label': train_data_labels})
+    valid_data = pd.DataFrame({'id': test_data_files, 'label': test_data_labels})
+    test_data = pd.DataFrame({'id': test_data_files, 'label': test_data_labels})
 
     unique, counts = np.unique(labels, return_counts=True)
     weights = torch.Tensor(sklearn.utils.class_weight.compute_class_weight(class_weight="balanced", classes=unique, y=np.asarray(labels)))
@@ -59,3 +61,21 @@ class CreateDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.entry)
+
+if __name__ == "__main__":
+    args = TrainingArgs()
+    data, weights = get_data(args)
+
+    args.train_file = data[0]
+    args.valid_file = data[1]
+    args.test_file = data[2]
+
+    train_dataset = CreateDataset(args, mode='train')
+    valid_dataset = CreateDataset(args, mode='valid')
+    test_dataset = CreateDataset(args, mode='test')
+
+    args.data = (train_dataset, valid_dataset, test_dataset)
+
+    print(len(train_dataset))
+    print(len(valid_dataset))
+    print(len(test_dataset))
